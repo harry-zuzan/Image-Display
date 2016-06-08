2c58a38f8c9be800aa9d37bd7d993e8c36bb9818
Harry Zuzan <iliketobicycle@gmail.com>
2016-06-07 23:16:38 -0700

    fixed filename typo

--- display.py
+++ display.py
@@ -6,36 +6,38 @@ from Tkinter import *
 from PIL import Image, ImageTk
 #import ttk
 
+from NameList import NameList
 
-class NameList:
-	def __init__(self,names):
-		self.names = list(names)
-		self.posn = 0
 
-	def __len__(self):
-		return len(self.names)
+#class NameList:
+#	def __init__(self,names):
+#		self.names = list(names)
+#		self.posn = 0
 
-	def current(self):
-		return self.names[self.posn]
+#	def __len__(self):
+#		return len(self.names)
 
-	def prev(self):
-		if self.posn > 0: self.posn -= 1
-		return self.names[self.posn]
+#	def current(self):
+#		return self.names[self.posn]
 
-	def next(self):
-		if self.posn < (len(self) - 1): self.posn += 1
-		return self.names[self.posn]
+#	def prev(self):
+#		if self.posn > 0: self.posn -= 1
+#		return self.names[self.posn]
 
-	def at_begin(self):
-		if self.posn == 0: return True
-		return False
+#	def next(self):
+#		if self.posn < (len(self) - 1): self.posn += 1
+#		return self.names[self.posn]
 
-	def at_end(self):
-		if self.posn == (len(self) - 1): return True
-		return False
+#	def at_begin(self):
+#		if self.posn == 0: return True
+#		return False
 
-	def at_middle(self):
-		return not (self.at_begin() or self.at_end())
+#	def at_end(self):
+#		if self.posn == (len(self) - 1): return True
+#		return False
+
+#	def at_middle(self):
+#		return not (self.at_begin() or self.at_end())
 
 
 
